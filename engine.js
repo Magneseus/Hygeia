5622256ccc06fe39bb397eb3502d5d5b8b8882ab
Matt Mayer <matthew.mayer@165gc.onmicrosoft.com>
2019-04-30 13:20:05 -0400

    Water & stone simulation
    Keyboard & mouse controls

--- engine.js
+++ engine.js
@@ -14,16 +14,24 @@
             this.height = canvas.height/this.scale;
             
             this.reset();
+            
+            this.currentSandType = 0;
+            this.newSandSpawn = null;
         }
         
         simTick() {
             let spawnVec = new iVec2D(Math.floor(this.width/2), 0);
             
-            if (this.gridList[spawnVec.x][spawnVec.y] !== null) {
-                this.reset();
+            if (this.gridList[spawnVec.x][spawnVec.y] === null) {
+                //this.reset();
+                this.createSand(spawnVec, 'sand');
             }
             
-            this.createSand(spawnVec, 'sand');
+            if (this.newSandSpawn !== null) {
+                this.createSand(this.newSandSpawn.pos, this.newSandSpawn.type);
+                this.newSandSpawn = null;
+            }
+            
             this.sandList.forEach((e) => {
                 e.simTick();
             });
@@ -34,9 +42,13 @@
         }
         
         createSand(pos, type) {
+            if (this.gridList[pos.x][pos.y] !== null) {
+                return;
+            }
+            
             let s = new Sand(
                 pos,
-                'sand',
+                type,
                 this.gridList,
                 this.gridImgData,
                 this.scale
@@ -44,18 +56,27 @@
             
             this.gridList[pos.x][pos.y] = s;
             this.sandList.push(s);
+            s.renderTick();
+        }
+        
+        onKeyboardDown(event) {
+            if (!event.repeat) {
+                if (event.key === '1') {
+                    this.currentSandType = 1;
+                } else if (event.key === '2') {
+                    this.currentSandType = 2;
+                } else if (event.key === '3') {
+                    this.currentSandType = 3;
+                } else if (event.key === '0') {
+                    this.currentSandType = 0;
+                }
+            }
         }
         
         onMouseDown(mousePos) {
-            
-        }
-        
-        onMouseUp(mousePos) {
-            
-        }
-        
-        onMouseMove(mousePos) {
-            
+            let spawnVec = new iVec2D(mousePos.x, mousePos.y);
+            spawnVec.scalarDiv(this.scale);
+            this.newSandSpawn = {pos: spawnVec, type: Sand.types[this.currentSandType] };
         }
         
         reset() {
@@ -144,8 +165,8 @@
         
         scalarDiv(scalar) {
             if (!Number.isInteger(scalar)) throw new Error('Invalid number type for iVec2D!');
-            this.x /= scalar;
-            this.y /= scalar;
+            this.x = Math.floor(this.x / scalar);
+            this.y = Math.floor(this.y / scalar);
         }
     }
     window.iVec2D = iVec2D;
@@ -165,16 +186,23 @@
         }
         
         checkBelow() {
+            if (this.type === 'stone') {
+                return this.pos;
+            }
+            
             let p = this.pos;
             let g = this.gridList;
             
-            if (p.y >= g[0].length) return this.pos;
+            if (p.y >= g[0].length-1) return this.pos;
             
             if (g[p.x][p.y+1] === null) {
                 return new iVec2D(p.x, p.y+1);
+            } else if (this.type === 'sand' && g[p.x][p.y+1].type === 'water') {
+                this.swapSand(g[p.x][p.y+1]);
+                return this.pos;
             }
             
-            if (p.x <= 0 || p.x >= g.length-1) return this.pos;
+            if (p.x <= 0 || p.x >= g.length-1) return this.pos;2
             
             let goLeftFirst = randGen.coinFlip();
             let xDelta = 1;
@@ -187,6 +215,14 @@
                 return new iVec2D(p.x + xDelta, p.y+1);
             } else if (g[p.x - xDelta][p.y+1] === null) {
                 return new iVec2D(p.x - xDelta, p.y+1);
+            }
+            
+            if (this.type === 'water') {
+                if (g[p.x + xDelta][p.y] === null) {
+                    return new iVec2D(p.x + xDelta, p.y);
+                } else if (g[p.x - xDelta][p.y] === null) {
+                    return new iVec2D(p.x - xDelta, p.y);
+                }
             }
             
             return this.pos;
@@ -205,6 +241,22 @@
             }
         }
         
+        swapSand(swSand) {
+            this.gridList[swSand.pos.x][swSand.pos.y] = this;
+            this.gridList[this.pos.x][this.pos.y] = swSand;
+            
+            this.setPixel(swSand.pos.x, swSand.pos.y, Sand.colors[this.type]);
+            this.setPixel(this.pos.x, this.pos.y, Sand.colors[swSand.type]);
+            
+            let oldPos = this.pos;
+            this.pos = swSand.pos;
+            swSand.pos = oldPos;
+        }
+        
+        renderTick() {
+            this.setPixel(this.pos.x, this.pos.y, Sand.colors[this.type]);
+        }
+        
         setPixel(x, y, col, size=this.pixelSize) {
             let newX = x * size;
             let newY = y * size;
@@ -219,12 +271,12 @@
             }
         }
     }
-    Sand.types  = ['sand', 'water', 'stone', 'none'];
+    Sand.types  = ['none', 'sand', 'water', 'stone'];
     Sand.colors = {
+        'none': [172, 177, 196],
         'sand': [231, 234, 18],
         'water': [28, 104, 219],
-        'stone': [58, 59, 63],
-        'none': [172, 177, 196]
+        'stone': [58, 59, 63]
     };
     window.Sand = Sand;
     
